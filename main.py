65fddf0780feaaf9d9a20e0a092793e00ebbd91a
Tungtwister <austin.tung56@gmail.com>
2024-12-23 10:36:49 -0800

    pulling data from RIOT API

--- main.py
+++ main.py
@@ -2,74 +2,34 @@ import requests
 import pandas as pd
 import config
 
-api_key = config.api_key
+API_KEY = config.api_key
 #Function to get player puuid, given a summoner name and region
-def get_puuid(summoner_name, region, api_key):
-    api_url = (
-        "https://" +
-        region +
-        ".api.riotgames.com/tft/summoner/v1/summoners/by-name/" +
-        summoner_name +
-        "?api_key=" +
-        api_key
-    )
 
-    print(api_url)
-    resp = requests.get(api_url)
-    player_info = resp.json()
-    puuid = player_info['puuid']
-    return puuid
+SUMMONER_URL = "https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/"
+MATCH_URL = "https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/"
+MATCH_DETAILS_URL = "https://americas.api.riotgames.com/tft/match/v1/matches/"
+ACCOUNT_URL = "https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/"
 
-#Function to get a list of all the match IDs, given a players puuid and mass region
-#totaling 20 match IDs
-def get_match_ids(puuid, mass_region, api_key):
-    api_url = (
-        "https://" +
-        mass_region +
-        ".api.riotgames.com/tft/match/v1/matches/by-puuid/" +
-        puuid + 
-        "/ids?start=0&count=20" + 
-        "&api_key=" + 
-        api_key
-    )
+def get_account(gameName, tagLine = "NA1"):
+    url = f"{ACCOUNT_URL}{gameName}/{tagLine}"
+    headers = {"X-Riot-Token": API_KEY}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+        return None
+
+def get_summoner_data(summoner_name):
+    url = f"{SUMMONER_URL}{summoner_name}"
+    headers = {"X-Riot-Token": API_KEY}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+        return None
     
-    print(api_url)
-    
-    resp = requests.get(api_url)
-    match_ids = resp.json()
-    return match_ids
-
-
-# From a given match ID and mass region, get the data about the game
-def get_match_data(match_id, mass_region, api_key):
-    api_url = (
-        "https://" + 
-        mass_region + 
-        ".api.riotgames.com/tft/match/v1/matches/" +
-        match_id + 
-        "?api_key=" + 
-        api_key
-    )
-    
-    resp = requests.get(api_url)
-    match_data = resp.json()
-    return match_data
-
-
-def gather_all_data(puuid, match_ids, mass_region, api_key):
-    # We initialise an empty dictionary to store data for each game
-    data = {
-        'traits': [],
-
-    }
-    
-    for match_id in match_ids:
-        print(match_id)
-        # run the two functions to get the player data from the   match ID
-        match_data = get_match_data(match_id, mass_region, api_key)
-        print(match_data[traits])
- 
-puuid = get_puuid("Leáf", "na1", api_key)
-match_ids = get_match_ids(puuid, "AMERICAS", api_key)
-print(match_ids)
-get_match_data(match_ids[0], "AMERICAS", api_key)
+# Example Usage
+account = get_account(gameName="Tungtwister",tagLine="Tung")
+print(account.get("puuid"))
